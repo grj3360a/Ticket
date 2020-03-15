a172750ca6156a1423e819f8888ce14cbe853fad
Ekinoxx <fadow0711@gmail.com>
2020-03-15 23:11:04 +0100

    Coronaworking 2

--- app/src/main/java/iut/ticket/CreditActivity.java
+++ app/src/main/java/iut/ticket/CreditActivity.java
@@ -3,9 +3,11 @@ package iut.ticket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class CreditActivity extends MenuedActivity {
 
@@ -15,4 +17,7 @@ public class CreditActivity extends MenuedActivity {
         setContentView(R.layout.activity_credit);
     }
 
+    public void onClickImage(View v) {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.iut_link))));
+    }
 }
